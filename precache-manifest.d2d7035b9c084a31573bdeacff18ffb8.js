d7285670c1d7b1a2d3cb7c84829b9a54f1bf9d14
Osamu Ito <mr.samito@gmail.com>
2023-02-09 16:31:05 -0500

    Updates

---
+++ precache-manifest.d2d7035b9c084a31573bdeacff18ffb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f14ef8fc223be0914a9",
+    "url": "/MovieApp/static/css/main.4153280a.chunk.css"
+  },
+  {
+    "revision": "5f14ef8fc223be0914a9",
+    "url": "/MovieApp/static/js/main.5f14ef8f.chunk.js"
+  },
+  {
+    "revision": "aad95fee85c18626c350",
+    "url": "/MovieApp/static/css/1.2ce78a85.chunk.css"
+  },
+  {
+    "revision": "aad95fee85c18626c350",
+    "url": "/MovieApp/static/js/1.aad95fee.chunk.js"
+  },
+  {
+    "revision": "5dfb1f54ad9e9ba2849e",
+    "url": "/MovieApp/static/js/runtime~main.5dfb1f54.js"
+  },
+  {
+    "revision": "3149298d6dc937914d2ecdddbe5ff059",
+    "url": "/MovieApp/index.html"
+  }
+];
