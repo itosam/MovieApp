3a07d5c8eb915945d57f9d9b825760db9f25ddf2
Osamu Ito <mr.samito@gmail.com>
2023-03-02 21:35:31 -0500

    updated home page and nav

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useEffect, useState } from "react";
 import LoadScreen from "./LoadScreen"
 import { Link } from "react-router-dom";
+import smd from "./smdlogo.png";
 //bootstrap import below
 import { Container } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
@@ -22,39 +23,53 @@ const Home = () => {
   }, []);
 
   return (
-    <Container
-      className="bg-light text-center"
-      style={{
-        border: "#4444 2px solid",
-        padding: "2%",
-        marginBottom: "2rem",
-        marginTop: "05%",
-        borderRadius: "30px",
-      }}
-    >
-      <h3 className="movie-box-heading">
-        <strong>Recent Releases:</strong>
-      </h3>
-  {isLoading && <LoadScreen/>}
-      {!isLoading}
-      {recentMovies.map((movie) => (
+    <div >
+      <div style={{paddingTop:"100px", textAlign:"center"}}>
         <img
-          className="home-poster"
-          alt="recent movie posters"
-          src={movie.poster_path}
-          style={{ width: "33%" }}
-        />
-      ))}
-      <Link to="/movies">
-        <Button
-          variant="outline-dark"
-          size="md"
-          style={{ border: "solid 2px", margin: "1rem" }}
-        >
-          Check out all Movies
-        </Button>
-      </Link>
-    </Container>
+        style={{width: "100px",height:"auto",  backgounrdColor: "white"}}
+        alt="slate img"
+        src={smd}
+      />
+      <row>
+      <h1 style={{}}>The Slate Movie Database</h1>
+      <h5>Our personal movie database for all new movies</h5>
+      </row>
+      </div>
+      
+      <Container
+        className="bg-light text-center"
+        style={{
+          border: "#4444 2px solid",
+          padding: "2%",
+          marginBottom: "2rem",
+          marginTop: "05%",
+          borderRadius: "30px",
+        }}
+      >
+        <h3 className="movie-box-heading">
+          <strong>Recent Releases:</strong>
+        </h3>
+        {isLoading && <LoadScreen />}
+        {!isLoading}
+        {recentMovies.map((movie) => (
+          <img
+            className="home-poster"
+            alt="recent movie posters"
+            src={movie.poster_path}
+            style={{ width: "33%" }}
+          />
+        ))}
+        <Link to="/movies">
+          <Button
+            variant="outline-dark"
+            size="md"
+            style={{ border: "solid 2px", margin: "1rem" }}
+          >
+            Check out all Movies
+          </Button>
+        </Link>
+      </Container>
+    </div>
   );
 };
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,24 +1,16 @@
 import React from "react";
 import { NavLink, Link } from "react-router-dom";
 import { Nav, Navbar, Container, Row, Col, Badge } from "react-bootstrap";
+
 function NavBar() {
   return (
     //made changeas to positionig of navbar and made it fixed to top of screen
     <header>
-      <Navbar bg="dark" fixed="top">
+      <Navbar bg="dark" fixed="top" className="justify-content-center">
         <div className="navigation">
           <Nav>
             <Container>
               <Row>
-                <Col>
-                  <Badge bg="" style={{ paddingRight: "500" }}>
-                    <Nav.Item>
-                      <Link to="/movieapp" style={{ color:"white",textDecoration: "none" }}>
-                        THE SLATE MOVIE DATABASE
-                      </Link>
-                    </Nav.Item>
-                  </Badge>
-                </Col>
                 <Col>
                   <Badge pill bg="light" style={{ padding: "10px 20px" }}>
                     <Nav.Item>
