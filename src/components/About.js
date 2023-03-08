f4cb38ac432704a7d18c7997a73d0c5d8898015f
Osamu Ito <mr.samito@gmail.com>
2023-03-08 14:45:44 -0500

    added icons to about info

--- src/components/About.js
+++ src/components/About.js
@@ -2,6 +2,8 @@ import React from "react";
 import smd from "./smdlogo.png";
 //booststrap imports below
 import { Row, Card } from "react-bootstrap";
+import { RiMovie2Fill } from "react-icons/ri";
+
 
 function About() {
   return (
@@ -41,18 +43,32 @@ function About() {
                 />
               </h1>
               <h4>
-                The Slate Movie Data base is a personal movie data base that will allow
-                users to search for movies, look at movie details, and add new
-                movies hosted on a JSON server. Users can also add/remove movies to a "favorites" list based on their tastes. </h4><br/> 
+                The Slate Movie Data base is a personal movie data base that
+                will allow users to search for movies, look at movie details,
+                and add new movies hosted on a JSON server. Users can also
+                add/remove movies to a "favorites" list based on their tastes.{" "}
+              </h4>
+              <br />
 
-                <h5>This application features:</h5>
-                <ul style={{listStyleType:"none"}}>
-                <li>Data fetching via JSON server</li>
-                <li>Client Side routing with React Routing</li>
-                <li>Search functionality using useState/useEffects</li>
-                <li>Styling and formatting with Bootstrap and CSS</li>
-                </ul>
-           
+              <h5>This application features:</h5>
+              <ul style={{ listStyleType: "none" }}>
+                <li>
+                  <RiMovie2Fill style={{ fontSize: "22px", color: "black" }} />
+                  {""} Data fetching via JSON server
+                </li>
+                <li>
+                  <RiMovie2Fill style={{ fontSize: "20px", color: "black" }} />
+                  {""} Client Side routing with React0Routing
+                </li>
+                <li>
+                  <RiMovie2Fill style={{ fontSize: "20px", color: "black" }} />
+                  {""} Search functionality using useState/useEffects
+                </li>
+                <li>
+                  <RiMovie2Fill style={{ fontSize: "20px", color: "black" }} />
+                  {""} Styling and formatting with Bootstrap and CSS
+                </li>
+              </ul>
             </Card.Body>
           </Card>
         }
